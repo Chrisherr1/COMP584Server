75e0e7bac18841579f18daeda1980d60ffebe498
ch423002 <ch423002@ECS1538-21>
2025-09-19 17:34:49 -0700

    added app.UseCors(options ==> ect ect)

--- COMP584Server/Program.cs
+++ COMP584Server/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen(options =>
@@ -28,7 +30,7 @@ if (app.Environment.IsDevelopment())
         // options.RoutePrefix = string.Empty;
     });
 }
-
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseAuthorization();
 
 app.MapControllers();
